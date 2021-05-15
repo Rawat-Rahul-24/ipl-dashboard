b24f5dc28589cce13be0052f79ba974f405b65ed
Rahul <rawatr0799@gmail.com>
2021-05-15 20:42:34 +0530

    Match Page added frontend

--- src/frontend/src/page/MatchPage.js
+++ src/frontend/src/page/MatchPage.js
@@ -5,10 +5,25 @@ import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
 
+    const [matches, setMatches] = useState([]);
+    const { teamName, year } = useParams();
+
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                const respone = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data = await respone.json();
+                setMatches(data);
+            };
+
+            fetchMatches();
+        }, [teamName]
+    )
    
     return (
         <div className="TeamPage">
-       <h1>Match Page</h1>
+        <h1>Match Page</h1>
+        {matches.map(match => <MatchDetailCard match={match} teamName = {teamName}/>)}
         </div>
     );
     }
